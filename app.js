e7b648add719aeadc31a2a1b3d6b11b2466f1a1f
Kenechukwu <kenechukwu.ajufo@gmail.com>
2021-09-23 00:14:53 +0100

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -11,10 +12,14 @@ app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true})
 
-userSchema = {
+userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "ThisnaOurLittleSecret";
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
