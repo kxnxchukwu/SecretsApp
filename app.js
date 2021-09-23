d84934cd03d6b4b554b8435030d35a5af0f28159
Kenechukwu <kenechukwu.ajufo@gmail.com>
2021-09-24 00:17:56 +0100

    Add Submit Secret Functionality

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
 userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -89,17 +90,50 @@ app.get('/register', function(req, res){
 
 app.get('/secrets', function(req, res) {
 
+    User.find({secret: {$ne: null}}, function(err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+
+});
+
+app.get('/submit', function(req, res) {
+
     if (req.isAuthenticated()) {
-        res.render('secrets');
+        res.render('submit');
     } else {
         res.redirect('/login');
     }
+
 });
 
 app.get('/logout', function(req, res) {
 
     req.logout();
     res.redirect('/');
+});
+
+app.post('/submit', function(req, res) {
+
+    User.findById(req.user._id, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                console.log(foundUser);
+                foundUser.secret = req.body.secret;
+                foundUser.save(function(){
+                    res.redirect('/secrets')
+                });
+            }
+        }
+    });
+
 });
 
 app.post('/register', function(req, res){
